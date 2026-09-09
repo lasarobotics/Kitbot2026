33364e5e0350f37fb8d62853406ed37d8ead6d5b
Alex <axu6088@gmail.com>
2026-09-09 18:16:09 -0500

    added name and mood to robot controller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,7 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
   public Robot() {
+    // alex :)
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
