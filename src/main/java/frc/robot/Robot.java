486bdf32349ebba80eee6d5e1586842389d431cc
Aadyan Lakku <aadyanreddy4@gmail.com>
2026-09-09 18:28:06 -0500

    added next 4 letters to alphabet

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,6 +26,7 @@ public class Robot extends TimedRobot {
   public Robot() {
     // abcd
     // efgh
+    // ijkl
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
