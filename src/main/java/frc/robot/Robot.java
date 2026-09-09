140f8d30963350bb0b39fb97585442f98430b4c6
notdyamuh <me@dyamuh.dev>
2026-09-09 18:21:00 -0500

    switch order to the comment to the constructor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,7 @@ public class Robot extends TimedRobot {
    */
   public Robot() {
     // noasdasdasd
+    // asdasdasdno
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
