0ebe2a724102c5342a6d87f7129a4a3fc57e0c7f
LASA Robotics Driver Station <driverstation@lasarobotics.org>
2026-09-09 18:22:01 -0500

    add an answer to the joke

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,6 +25,7 @@ public class Robot extends TimedRobot {
    */
   public Robot() {
     // what did the cow say to the bat?
+    // moo
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
