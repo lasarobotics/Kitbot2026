26c0ddf661551b5709c0c79f53e7949a4d79d20b
LASA Robotics Driver Station <driverstation@lasarobotics.org>
2026-09-16 18:59:19 -0500

    added fuel controller stuff to robot.java and itself

--- src/main/java/frc/robot/subsystems/FuelControllerSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelControllerSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import java.util.function.BooleanSupplier;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -8,6 +10,10 @@ public class FuelControllerSubsystem extends SubsystemBase {
   private static FuelControllerSubsystem s_fuelControllerSubsystem;
   TalonFX m_leftMainMotor;
   TalonFX m_rightMainMotor;
+
+  BooleanSupplier m_shouldShoot;
+  BooleanSupplier m_shouldIntake;
+  BooleanSupplier m_shouldReverse;
 
   public static FuelControllerSubsystem getInstance() {
     if (s_fuelControllerSubsystem == null) {
@@ -20,4 +26,30 @@ public class FuelControllerSubsystem extends SubsystemBase {
     m_leftMainMotor = new TalonFX(30);
     m_rightMainMotor = new TalonFX(31);
   }
-}
+
+  public void configureBindings(
+    BooleanSupplier shouldShoot, BooleanSupplier shouldIntake, BooleanSupplier shouldReverse) {
+      m_shouldShoot = shouldShoot;
+      m_shouldReverse = shouldReverse;
+      m_shouldIntake = shouldIntake;
+    }
+  
+
+  @Override
+  public void periodic() {
+    if (m_shouldShoot.getAsBoolean()) {
+      m_leftMainMotor.set(1.0);
+      m_rightMainMotor.set(-1.0);
+    } else if (m_shouldIntake.getAsBoolean()) {
+      m_leftMainMotor.set(1.0);
+      m_rightMainMotor.set(1.0);
+    } else if (m_shouldReverse.getAsBoolean()) {
+      m_leftMainMotor.set(-1.0);
+      m_rightMainMotor.set(-1.0);
+    } else {
+      m_leftMainMotor.set(0.0);
+      m_rightMainMotor.set(0.0);
+    }
+    }
+  }
+
