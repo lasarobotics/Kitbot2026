d288d56024e3b42ed0bccf1c016b8e61c122c9d3
Alex <axu6088@gmail.com>
2026-09-17 18:31:48 -0500

    Added 676767 to the FuelControllerSubsystem and made it easier to read with new methods.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+
+public class Constants {
+  public static class FuelController {
+    public static final AngularVelocity SHOOTING_SHOOTER_SPEED = RotationsPerSecond.of(40);
+    public static final AngularVelocity SHOOTING_INDEXER_SPEED = RotationsPerSecond.of(-20);
+
+    public static final AngularVelocity INTAKING_SHOOTER_SPEED = RotationsPerSecond.of(30);
+    public static final AngularVelocity INTAKING_INDEXER_SPEED = RotationsPerSecond.of(40);
+
+    public static final AngularVelocity REVERSING_SHOOTER_SPEED = RotationsPerSecond.of(-30);
+    public static final AngularVelocity REVERSING_INDEXER_SPEED = RotationsPerSecond.of(-30);
+
+    public static final AngularVelocity SHOOTER_SPEED_ERROR_TOLERANCE = RotationsPerSecond.of(2);
+  }
+}
--- src/main/java/frc/robot/subsystems/FuelControllerSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelControllerSubsystem.java
@@ -1,14 +1,19 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import java.util.function.BooleanSupplier;
 
 public class FuelControllerSubsystem extends SubsystemBase {
 
   public static FuelControllerSubsystem s_fuelControllerSubsystem;
-  TalonFX m_leftMainMotor;
-  TalonFX m_rightMainMotor;
+  TalonFX m_shooterIntakeMotor;
+  TalonFX m_indexerMotor;
+
+  private final VelocityVoltage m_shooterIntakeRequest;
+  private final VelocityVoltage m_indexerRequest;
 
   BooleanSupplier m_shouldShoot;
   BooleanSupplier m_shouldIntake;
@@ -22,8 +27,11 @@ public class FuelControllerSubsystem extends SubsystemBase {
   }
 
   public FuelControllerSubsystem() {
-    m_leftMainMotor = new TalonFX(30);
-    m_rightMainMotor = new TalonFX(31);
+    m_shooterIntakeMotor = new TalonFX(30);
+    m_indexerMotor = new TalonFX(31);
+
+    m_shooterIntakeRequest = new VelocityVoltage(0);
+    m_indexerRequest = new VelocityVoltage(0);
   }
 
   public void configureBindings(
@@ -37,17 +45,66 @@ public class FuelControllerSubsystem extends SubsystemBase {
   public void periodic() {
 
     if (m_shouldShoot.getAsBoolean()) {
-      m_leftMainMotor.set(1.0);
-      m_rightMainMotor.set(-1.0);
+      runShooter();
+
+      if (atShootingSpeed()) {
+        runIndexer();
+      } else {
+        stopIndexer();
+      }
+
     } else if (m_shouldIntake.getAsBoolean()) {
-      m_leftMainMotor.set(1.0);
-      m_rightMainMotor.set(1.0);
+      setMotorsIntake();
     } else if (m_shouldReverse.getAsBoolean()) {
-      m_leftMainMotor.set(-1.0);
-      m_rightMainMotor.set(-1.0);
+      setMotorsReverse();
     } else {
-      m_leftMainMotor.set(0);
-      m_rightMainMotor.set(0);
+      stopMotors();
     }
+    // 676767
+  }
+
+  private boolean atShootingSpeed() {
+    return m_shooterIntakeMotor
+        .getVelocity()
+        .isNear(
+            Constants.FuelController.SHOOTING_SHOOTER_SPEED,
+            Constants.FuelController.SHOOTER_SPEED_ERROR_TOLERANCE);
+  }
+
+  private void runShooter() {
+    m_shooterIntakeMotor.setControl(
+        m_shooterIntakeRequest.withVelocity(Constants.FuelController.SHOOTING_SHOOTER_SPEED));
+  }
+
+  private void runIndexer() {
+    m_indexerMotor.setControl(
+        m_indexerRequest.withVelocity(Constants.FuelController.SHOOTING_INDEXER_SPEED));
+  }
+
+  private void setMotorsIntake() {
+    m_shooterIntakeMotor.setControl(
+        m_shooterIntakeRequest.withVelocity(Constants.FuelController.INTAKING_SHOOTER_SPEED));
+    m_indexerMotor.setControl(
+        m_indexerRequest.withVelocity(Constants.FuelController.INTAKING_INDEXER_SPEED));
+  }
+
+  private void setMotorsReverse() {
+    m_shooterIntakeMotor.setControl(
+        m_shooterIntakeRequest.withVelocity(Constants.FuelController.REVERSING_SHOOTER_SPEED));
+    m_indexerMotor.setControl(
+        m_indexerRequest.withVelocity(Constants.FuelController.REVERSING_INDEXER_SPEED));
+  }
+
+  private void stopMotors() {
+    stopShooter();
+    stopIndexer();
+  }
+
+  private void stopShooter() {
+    m_shooterIntakeMotor.stopMotor();
+  }
+
+  private void stopIndexer() {
+    m_indexerMotor.stopMotor();
   }
 }
