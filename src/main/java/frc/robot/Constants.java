95497d2ad8102c1f53149f8684b156d4a6e1e75f
Alex <axu6088@gmail.com>
2026-09-24 17:28:40 -0500

    Added Auto and 676767 to Robot.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,8 +1,10 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
 
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Time;
 
 public class Constants {
   public static class FuelController {
@@ -16,5 +18,13 @@ public class Constants {
     public static final AngularVelocity REVERSING_INDEXER_SPEED = RotationsPerSecond.of(-30);
 
     public static final AngularVelocity SHOOTER_SPEED_ERROR_TOLERANCE = RotationsPerSecond.of(2);
+  }
+
+  public static class Auto {
+    public static final Time AUTO_DRIVE_TIME = Seconds.of(2);
+    public static final Time AUTO_SHOOT_TIME = Seconds.of(2);
+    public static final Time TOTAL_AUTO_TIME = AUTO_DRIVE_TIME.plus(AUTO_SHOOT_TIME);
+
+    public static final double AUTO_DRIVE_SPEED = 0.5;
   }
 }
